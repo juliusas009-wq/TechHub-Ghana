2b1f6af25defac12be873d9096a124252bb6ec98
juliusas009-wq <juliusas009@gmail.com>
2026-07-13 10:31:54 +0000

    Add files via upload

--- live-news.js
+++ live-news.js
@@ -1,4 +1,33 @@
-import {
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import { 
+    getFirestore,
+    collection,
+    getDocs
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+
+// Firebase Config
+const firebaseConfig = {
+
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_PROJECT.firebaseapp.com",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_PROJECT.appspot.com",
+    messagingSenderId: "YOUR_SENDER_ID",
+    appId: "YOUR_APP_ID"
+
+};
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+// THIS CREATES db
+const db = getFirestore(app);
+import  {
+
     collection,
     getDocs
 } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
